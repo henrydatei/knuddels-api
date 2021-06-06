58919e4fb6d4543aa88bfb7d895891488091a740
Henry Haustein <henrydatei@web.de>
2021-06-06 17:21:47 +0200

    some test code

--- test.py
+++ test.py
@@ -4,5 +4,16 @@ import json
 
 username = "yourUsername"
 password = "yourPassword"
+sessionToken = login(username,password)
+# print(sessionToken)
+persons = getPersons(sessionToken)
+# print(getConversation(sessionToken, "276232853228727893"))
+print(len(persons))
+for person in persons:
+    messages = getAllMessagesPerConversation(sessionToken, person.conversationID)
+    print(person.id + " - " + person.nick + " - " + str(len(messages)) + " Nachrichten")
 
-print(login(username,password))
+# messages = getAllMessagesPerConversation(sessionToken, "261426327137922645")
+# for message in messages:
+#     print(message.senderID + " - " + message.formattedText.text)
+# print(len(messages))
